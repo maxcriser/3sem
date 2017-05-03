c85cc4fc5ffe0a3afb319892cbbd4d6a5db48829
maxcriser <mv.maxcriser@gmail.com>
2017-05-04 01:01:22 +0300

    фвв

---
+++ OperatingSystem/Threads/MultiThreads/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreads
+{
+    class Program
+    {
+        private static void calculate()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            int N = 10000;
+            int K = 10000;
+            int countThreads = 4;
+
+            double[] a = new double[N];
+            double[] b = new double[N];
+
+            Console.WriteLine("Start: " + DateTime.UtcNow.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+
+            for (int c = 0; c < countThreads; c++)
+            {
+                int start = c * N / countThreads;
+                int end = (c + 1) * N / countThreads;
+
+                Thread thr2 = new Thread(() =>
+                {
+                    for (int i = start; i < end; i++)
+                    {
+                        for (int j = 0; j < K; j++)
+                        {
+                            b[i] += Math.Pow(a[i], 1.789);
+                        }
+                    }
+                });
+
+                thr2.Start();
+
+                if (c == countThreads - 1)
+                {
+                    thr2.Join();
+                }
+            }
+
+            Console.WriteLine("Stop: " + DateTime.UtcNow.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperatingSystem/Threads/Threads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5000;
+            int K = 5000;
+            double[] a = new double[N];
+            double[] b = new double[N];
+           
+            Console.WriteLine("Start: " + DateTime.UtcNow.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    b[i] += Math.Pow(a[i], 1.789);
+                }
+            }
+            Console.WriteLine("Stop: " + DateTime.UtcNow.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
